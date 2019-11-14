ac5beae7294fda4a56a8a75ddd38ae28aaa69b31
Divya Ann Kurien <divya.kurien@gmail.com>
2019-11-14 08:25:46 +0530

    Assembler pass1 added

--- ass2pass.c
+++ ass2pass.c
@@ -3,18 +3,19 @@
 #include<string.h>
 void main()
 {
-	char label[10],opcode[10],operand[10],ch,pseudo_opcode[10][10]={"START","LDA","STA","LDCH","STCH","END"};
+	char label[10],opcode[10],operand[10],code[10],nmemonics[10];
 	int locctr,i=0,error,start_add,length;
-	FILE *f1,*f2,*f3;
+	FILE *f1,*f2,*f3.*f4;
 	f1 = fopen("input.txt","r");
-	f2 = fopen("symtab.txt","w");
-	f3 = fopen("output.txt","w");
+	f3 = fopen("symtab.txt","w");
+	f2 = fopen("optab.txt","w");
+	f4 = fopen("output.txt","w");
 	fscanf(f1,"%s%s%s",label,opcode,operand);
 	if(strcmp(opcode,"START")==0)
 	{
 		locctr = atoi(operand);
 		start_add = locctr;
-		fprintf(f3,"%s\t%s\t%s\n",label,opcode,operand);
+		fprintf(f4,"%s\t%s\t%s\n",label,opcode,operand);
 		fscanf(f1,"%s%s%s",label,opcode,operand);
 	}
 	else
@@ -22,21 +23,23 @@ void main()
 		locctr=0;
 		start_add=0;
 	}
+	
 	while(strcmp(opcode,"END")!=0)
-	{   fprintf(f3,"%d",locctr);
+	{   fprintf(f4,"%d",locctr);
+	
 		if(strcmp(label,"**")!=0)
 		{
-			fprintf(f2,"%s\t%d\n",label,locctr);
+			fprintf(f3,"%s\t%d\n",label,locctr);
 		}
-		
-		while(strcmp(pseudo_opcode[i],"END")!=0)
+		fscanf(f2,"%s%s",code,nmemonic);
+		while(strcmp(code,"END")!=0)
     	{
-     		if(strcmp(opcode,pseudo_opcode[i])==0)
+     		if(strcmp(opcode,code)==0)
      		{
       			locctr+=3;
       			break;
      		}
-     		i++;
+     		fscanf(f2,"%s%s",code,nmemonic);
     	}
 		if(strcmp(opcode,"WORD")==0)
 			locctr= locctr+3;
@@ -51,37 +54,19 @@ void main()
 			locctr = locctr + atoi(operand)*3;
 		else if(strcmp(opcode,"RESB")==0)
 			locctr = locctr+atoi(operand);
+			
+		fprintf("%s\t%s\t%s",label,opcode,operand);
+		fscanf("%s\t%s\t%s",label,opcode,operand);
 		
 	}
-	fprintf(f3,"%d\t%s\t%s\t%s\n",locctr,label,opcode,operand);
+	fprintf(f4,"%d\t%s\t%s\t%s\n",locctr,label,opcode,operand);
 	length = locctr - start_add;
 	printf("\nLength of the program is %d",length);
-	fcloseall();
-	printf("\n\nInput file :\n\n");
-    f1=fopen("input.txt","r");
-    ch=fgetc(f1);
-    while(ch!=EOF)
-    {
-    	printf("%c",ch);
-    	ch=fgetc(f1);
-   	}
-  	printf("\nTemporary file :\n\n\t");
-    f3=fopen("output.txt","r");
-    ch=fgetc(f3);
-    while(ch!=EOF)
-    {
-    	printf("%c",ch);
-    	ch=fgetc(f3);
-    }
-  	printf("\nSymbol Table(Symtab) :\n\n");
-    f2=fopen("symtab.txt","r");
-    ch=fgetc(f2);
-  	while(ch!=EOF)
-   	{
-    	printf("%c",ch);
-    	ch=fgetc(f2);
-    }
-  fcloseall();
+	fclose(f1);
+	fclose(f2);
+	fclose(f3);
+	fclose(f4);
+  
 }
 	
 
